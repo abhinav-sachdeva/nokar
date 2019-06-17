0ce9971e3dfa260b93918c6e4edb761044997b2f
happy257@gmail.com <happy257@gmail.com>
2019-06-17 14:17:07 +0530

    Prefix suffix for responses

--- services/RandomResponse.js
+++ services/RandomResponse.js
@@ -1,13 +1,31 @@
-exports.get = (replyRef) =>{
-    let replyRefType = typeof replyRef;
-    
-    if(replyRefType==="string") return replyRef;
+exports.get = replyRef => {
+  let replyRefType = typeof replyRef;
 
-    if ((replyRefType !=="object") || 
-    (replyRefType ==="object" && replyRef.length==undefined) ||
-    (replyRefType ==="object" && !replyRef.length)){
-      return false;
-    }
+  if (replyRefType === "string") return replyRef;
 
-    return replyRef[Math.floor(Math.random()*replyRef.length)];  
+  if (
+    replyRefType !== "object" ||
+    (replyRefType === "object" && replyRef.length == undefined) ||
+    (replyRefType === "object" && !replyRef.length)
+  ) {
+    return false;
   }
+
+  let randomElem = replyRef[Math.floor(Math.random() * replyRef.length)];
+  let responseText = null;
+  const elemType = typeof randomElem;
+  if (elemType === "string") return randomElem;
+  if (elemType === "object" && !(randomElem.prefix || randomElem.suffix))
+    return randomElem.text;
+  if (elemType === "object" && randomElem.prefix) {
+    responseText = `${randomElem.prefix[
+      Math.floor(Math.random() * randomElem.prefix.length)
+    ]} ${responseText}`;
+  }
+  if (elemType === "object" && randomElem.suffix) {
+    responseText = `${responseText} ${randomElem.suffix[
+      Math.floor(Math.random() * randomElem.suffix.length)
+    ]}`;
+  }
+  return responseText;
+};
